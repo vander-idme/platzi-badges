c6c8fac066d22c5a4b6ac892919c31de8c142b15
vander-idme <idmevander@gmail.com>
2020-08-16 16:34:54 -0500

    Con azucar sintactico

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// const element = document.createElement('h1')
+// element.innerText = "Hello, Vander"
 
-// const container = document.getElementById('app');
+// const container = document.getElementById('app')
 
-// container.appendChild(element);
+// container.appendChild(element)
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hola Vander</h1>
+const container = document.getElementById('app')
 
-const container = document.getElementById('app');
-
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// ReactDOM.render(__qué__, __dónde__)
+ReactDOM.render(element, container)
