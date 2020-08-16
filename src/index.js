46f92fa7b85d47002807b9213732a80a1572e312
vander-idme <idmevander@gmail.com>
2020-08-16 17:24:37 -0500

    Sin azucar sintactico, React es javascript

--- src/index.js
+++ src/index.js
@@ -5,11 +5,34 @@
 
 // container.appendChild(element)
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hola Vander</h1>
-const container = document.getElementById('app')
+// const jsx = <h1>Hola Vander</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a platzi"
+// );
+// const name = "Zhaida";
+// const sum = () => 3 * 3;
 
+// const element = React.createElement("h1", {}, `Hola soy ${name}`);
+// const jsx = <h1>Hola soy, {sum()}</h1>;
+
+const jsx = (
+  <div>
+    <h1>Hola, soy Vander</h1>
+    <p>Soy estudiante</p>
+  </div>
+);
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Vander"),
+  React.createElement("p", {}, "Soy estudiante")
+);
+const container = document.getElementById("app");
 // ReactDOM.render(__qué__, __dónde__)
-ReactDOM.render(element, container)
+ReactDOM.render(element, container);
