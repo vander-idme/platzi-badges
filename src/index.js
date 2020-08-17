a2a2736faf353afdf9b03d00224185a3d8100fd1
vander-idme <idmevander@gmail.com>
2020-08-17 18:03:15 -0500

    volviendo al estado inicial del proyecto

--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
-// const element = document.createElement('h1')
-// element.innerText = "Hello, Vander"
-
-// const container = document.getElementById('app')
-
-// container.appendChild(element)
-
-import React from "react";
+import React from "reac";
 import ReactDOM from "react-dom";
 
-// const jsx = <h1>Hola Vander</h1>;
-// const element = React.createElement(
-//   "a",
-//   { href: "https://platzi.com" },
-//   "Ir a platzi"
-// );
-// const name = "Zhaida";
-// const sum = () => 3 * 3;
-
-// const element = React.createElement("h1", {}, `Hola soy ${name}`);
-// const jsx = <h1>Hola soy, {sum()}</h1>;
-
-const jsx = (
+const element = (
   <div>
     <h1>Hola, soy Vander</h1>
-    <p>Soy estudiante</p>
+    <p>Soy estudiante de programacion</p>
   </div>
 );
 
-const element = React.createElement(
-  "div",
-  {},
-  React.createElement("h1", {}, "Hola, soy Vander"),
-  React.createElement("p", {}, "Soy estudiante")
-);
 const container = document.getElementById("app");
-// ReactDOM.render(__qué__, __dónde__)
+
 ReactDOM.render(element, container);
