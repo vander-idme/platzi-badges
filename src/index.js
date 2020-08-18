d179bbafb3cb63f2196907116008f39f161dc4c9
vander-idme <idmevander@gmail.com>
2020-08-18 18:41:07 -0500

    Primera pagina

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import BadgeNew from "./pages/BadgeNew";
 
 import "bootstrap/dist/css/bootstrap.css";
-
 import "./global.css";
-import Badge from "./components/Badge";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Zhaida"
-    lastName="Cazasola"
-    email="idmevander@gmail.com"
-    jobTitle="Kickass"
-    twitter="zhaidaCazasola"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
