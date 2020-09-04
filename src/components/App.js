bb7671f741a394a05582c8d14b692741bc592165
vander-idme <idmevander@gmail.com>
2020-09-04 16:44:57 -0500

    Agregando el NotFound

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
 import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
+import NotFound from "../pages/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route component={NotFound} />
         </Switch>
       </Layout>
     </BrowserRouter>
