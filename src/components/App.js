68d96d6e600e09240ebfd547d13bba1b2b2bc73a
vander-idme <idmevander@gmail.com>
2020-09-04 18:10:24 -0500

    Haciendo Home Page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Layout from "./Layout";
+import Home from "../pages/Home";
 import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
 import NotFound from "../pages/NotFound";
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route component={NotFound} />
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import logoConf from "../images/platziconf-logo.svg";
+import astronauts from "../images/astronauts.svg";
+
+import "./styles/Home.css";
+import { Link } from "react-router-dom";
+function Home() {
+  return (
+    <div className="Home">
+      <div className="Description__container">
+        <img className="Description__image" src={logoConf} />
+        <h1 className="Description__title">Print your Badges</h1>
+        <p className="Description__paragraph">
+          The easiest way to manage your conference
+        </p>
+
+        <Link to="/badges/new" className="Description__button">
+          Start Now
+        </Link>
+      </div>
+      <div className="Image__container">
+        <img src={astronauts} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
