f24f68ed98e68c5b591d7fe1747ca8a31bb70e7d
vander-idme <idmevander@gmail.com>
2020-09-03 19:22:00 -0500

    Implementando ReactRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BadgeNew from "./pages/BadgeNew";
-import Badges from "./pages/Badges";
+import App from "./components/App";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<Badges />, container);
+ReactDOM.render(<App />, container);
